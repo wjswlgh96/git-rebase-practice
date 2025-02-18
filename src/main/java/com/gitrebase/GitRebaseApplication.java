8955a12162169b038a9d53085a8564d2f00d4680
JiHo Jeon <wjswlgh96@naver.com>
2025-02-18 13:26:07 +0900

    first commit

---
+++ src/main/java/com/gitrebase/GitRebaseApplication.java
@@ -0,0 +1,13 @@
+package com.gitrebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitRebaseApplication.class, args);
+    }
+
+}
